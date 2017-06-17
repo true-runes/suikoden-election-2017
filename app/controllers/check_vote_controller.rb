a5f577b12d1be2987ddca72b56aad52cedbd4f9a
corselia <takiya@toran.sakura.ne.jp>
2017-06-17 11:37:32 +0900

    [update] update check_vote conditions
    contain not only '幻水総選挙2017投票' but also '幻水総選挙2017'

--- app/controllers/check_vote_controller.rb
+++ app/controllers/check_vote_controller.rb
@@ -9,6 +9,6 @@ class CheckVoteController < ApplicationController
   def result
     screen_name = params[:screen_name].nil? ? nil : params[:screen_name].gsub(/@/, "")
     tweets_by_specific_user = tweets_by_specific_user(screen_name)
-    @result_tweets = select_tweets_by_hashtag(tweets_by_specific_user, "幻水総選挙2017投票")
+    @result_tweets = select_tweets_by_hashtag(tweets_by_specific_user, "幻水総選挙2017投票", "幻水総選挙2017")
   end
 end
--- app/controllers/concerns/select_tweets_by_hashtag.rb
+++ app/controllers/concerns/select_tweets_by_hashtag.rb
@@ -1,11 +1,11 @@
 module SelectTweetsByHashtag
-  # HACK: 引数 hashtag は、複数の値を取れるようにする
-  def select_tweets_by_hashtag(tweets, hashtag)
+  # HACK: 引数 hashtag は、複数の値を取れるようにする（現状は場当たり的にも程がある）
+  def select_tweets_by_hashtag(tweets, hashtag_a, hashtag_b="")
     @selected_tweets = []
     tweets.each do |tweet|
       target_tweets = Hashtag.where(tweet_id: tweet.tweet_id)
       target_tweets.each do |target_tweet|
-        @selected_tweets << tweet if target_tweet.tagname == hashtag
+        @selected_tweets << tweet if target_tweet.tagname == hashtag_a or target_tweet.tagname == hashtag_b
       end
     end
     @selected_tweets
