145bcf85dc05aada6480e0ccf39d7e8d7a48a4dc
corselia <takiya@toran.sakura.ne.jp>
2017-06-26 21:24:44 +0900

    [update] update description a little
    update description a little

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,21 +3,21 @@ class IndexController < ApplicationController
   include SelectTweetsByHashtag
 
   def index
-    source_tweets = without_retweets_and_gensosenkyo_loose_limited_period
+    # source_tweets = without_retweets_and_gensosenkyo_loose_limited_period
 
     # TODO: コントローラに詰め込みすぎ
     # TODO: ここでいちいち配列に入れて、その値を元にツイートを選別……というのが速度低下の要因と思われる
-    unreadable_tweet_ids = IsReadableTweet.where(is_readable: false)
-    @removed_tweet_ids = []
-    unreadable_tweet_ids.each do |tweet|
-      @removed_tweet_ids << tweet.tweet_id
-    end
+    # unreadable_tweet_ids = IsReadableTweet.where(is_readable: false)
+    # @removed_tweet_ids = []
+    # unreadable_tweet_ids.each do |tweet|
+    #   @removed_tweet_ids << tweet.tweet_id
+    # end
 
-    amount_tweets = source_tweets.reject do |element|
-      @removed_tweet_ids.include?(element.tweet_id)
-    end
+    # amount_tweets = source_tweets.reject do |element|
+    #   @removed_tweet_ids.include?(element.tweet_id)
+    # end
 
-    @vote_amount = select_tweets_by_hashtag(amount_tweets, "幻水総選挙2017投票").length
+    # @vote_amount = select_tweets_by_hashtag(amount_tweets, "幻水総選挙2017投票").length
   end
 
   def thanks
