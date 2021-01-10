65c4321634cad4fe56761713e93df7a4e25378c7
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-10 23:21:25 +0900

    feat: 🎸 Stop using "secrets.yml" (#45)
    Stop using "secrets.yml" and start using "dotenv" and "credentials:edit"

--- app/controllers/for_members_normal_vote_controller.rb
+++ app/controllers/for_members_normal_vote_controller.rb
@@ -9,7 +9,7 @@ class ForMembersNormalVoteController < ApplicationController
 
   def site_http_basic_authenticate_with
     authenticate_or_request_with_http_basic("Hello, gensosenkyo staff!") do |username, password|
-      username == Rails.application.secrets.members_page_auth_username && password == Rails.application.secrets.members_page_auth_password
+      username == ENV['MEMBERS_PAGE_AUTH_USERNAME'] && password == ENV['MEMBERS_PAGE_AUTH_PASSWORD']
     end
   end
 
