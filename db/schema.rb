2482c231f054616c2c04efa6cfcb98e060b78ec9
Osamu Takiya <takiya@toran.sakura.ne.jp>
2021-01-10 22:23:31 +0900

    feat: 🎸 Remove "sqlite3" gem and other gems for Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618140954) do
+ActiveRecord::Schema.define(version: 2017_06_18_140954) do
 
-  create_table "attached_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "attached_images", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "tweet_id"
     t.string "media_id"
     t.string "uri"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20170618140954) do
     t.index ["media_id"], name: "index_attached_images_on_media_id", unique: true
   end
 
-  create_table "hashtags", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "hashtags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "tweet_id"
     t.string "tagname"
     t.datetime "created_at", null: false
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 20170618140954) do
     t.index ["tweet_id", "tagname"], name: "index_hashtags_on_tweet_id_and_tagname", unique: true
   end
 
-  create_table "is_readable_tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "is_readable_tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "tweet_id"
     t.boolean "is_readable"
     t.datetime "created_at", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20170618140954) do
     t.index ["tweet_id"], name: "index_is_readable_tweets_on_tweet_id", unique: true
   end
 
-  create_table "tweets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "tweets", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "tweet_id"
     t.string "user_id"
     t.string "text"
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 20170618140954) do
     t.index ["tweet_id"], name: "index_tweets_on_tweet_id", unique: true
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "user_id"
     t.string "screen_name"
     t.string "name"
