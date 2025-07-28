2773355e818744bbf58be9b1db48488a4c01edd9
Osamu Takiya <takiya@toran.sakura.ne.jp>
2025-07-29 08:24:56 +0900

    refactor: 💡 Remove unused test/

--- test/controllers/for_members_check_vote_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ForMembersCheckVoteControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/attached_image_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class AttachedImageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
